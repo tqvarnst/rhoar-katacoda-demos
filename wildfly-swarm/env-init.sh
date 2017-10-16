cd4deec510c03d90fde881892c578b61ed7016f9
Thomas Qvarnström <tqvarnst@redhat.com>
2017-10-16 20:27:51 +0200

    Testing adding roles

--- wildfly-swarm/env-init.sh
+++ wildfly-swarm/env-init.sh
@@ -1,1 +1,3 @@
 ssh root@host01 "oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/ose-4.5/jboss-image-streams.json -n openshift"
+ssh root@host01 "oadm new-project launcher --display-name='RHOAR Launcher'"
+ssh root@host01 "oadm policy add-role-to-user view developer -n launcher"
