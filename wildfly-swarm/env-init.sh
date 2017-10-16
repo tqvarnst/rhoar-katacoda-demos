13bdacc6373844bb1042a5b2e010a09c67c878aa
Thomas Qvarnström <tqvarnst@redhat.com>
2017-10-16 17:59:03 +0200

    Fixed the layout

--- wildfly-swarm/env-init.sh
+++ wildfly-swarm/env-init.sh
@@ -1,2 +1,1 @@
-ssh root@host01 'for i in {1..200}; do oc policy add-role-to-user system:image-puller system:anonymous && break || sleep 1; done'
 ssh root@host01 "oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/ose-4.5/jboss-image-streams.json -n openshift"
