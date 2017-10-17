4546c9ed0e80966b2533394ce315d3d89e96b057
Thomas Qvarnström <tqvarnst@redhat.com>
2017-10-17 09:23:24 +0200

    empty client

--- wildfly-swarm/client-init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo Logging into the OpenShift console as admin
-oc login -u system:admin > login-out.txt 2>&1
-
-
-echo "Importing Middleware imagestreams"
-oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/ose-v1.4.5/jboss-image-streams.json -n openshift
