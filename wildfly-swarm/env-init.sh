de90d4552826bd87ba6ba1d6aadc28abe360a7e1
Thomas Qvarnström <tqvarnst@redhat.com>
2017-10-17 15:25:33 +0200

    test

--- wildfly-swarm/env-init.sh
+++ wildfly-swarm/env-init.sh
@@ -1,3 +1,3 @@
 ssh root@host01 'for i in {1..200}; do oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/ose-v1.4.5/jboss-image-streams.json -n openshift && break || sleep 1; done'
-touch /root/testfile
+
 
