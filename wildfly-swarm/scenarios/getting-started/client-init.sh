29a1f028c974a76a0ca7b17956356def7fd5abdd
Thomas Qvarnström <tqvarnst@redhat.com>
2017-10-17 21:13:47 +0200

    test

--- wildfly-swarm/scenarios/getting-started/client-init.sh
+++ wildfly-swarm/scenarios/getting-started/client-init.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-echo Runnig client side script
-touch /root/testfile
+#echo Creating the Maven project
+
+#git clone https://github.com/wildfly-swarm-openshiftio-boosters/wfswarm-health-check.git my-first-project
+#git checkout 7.0.0-redhat-2
+
+#curl -L -o wfproject.zip https://github.com/wildfly-swarm-openshiftio-boosters/wfswarm-health-check/archive/7.0.0-redhat-3.zip
