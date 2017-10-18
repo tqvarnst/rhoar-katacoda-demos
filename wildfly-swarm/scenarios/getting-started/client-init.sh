c54e3532789671bc4c7e9c87ae96f321db64640e
Thomas Qvarnström <tqvarnst@redhat.com>
2017-10-18 14:40:52 +0200

    test

--- wildfly-swarm/scenarios/getting-started/client-init.sh
+++ wildfly-swarm/scenarios/getting-started/client-init.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#echo Creating the Maven project
 
-#git clone https://github.com/wildfly-swarm-openshiftio-boosters/wfswarm-health-check.git my-first-project
-#git checkout 7.0.0-redhat-2
+echo Installing Java
+yum install -y -q -e 0 java-1.8.0-openjdk-devel
 
-#curl -L -o wfproject.zip https://github.com/wildfly-swarm-openshiftio-boosters/wfswarm-health-check/archive/7.0.0-redhat-3.zip
+echo Installing Maven
+curl -s http://www-us.apache.org/dist/maven/binaries/apache-maven-3.2.2-bin.tar.gz | tar zx
+export PATH=$PATH:apache-maven-3.2.2/bin
 
-# echo Installing Maven
-# curl http://www-us.apache.org/dist/maven/binaries/apache-maven-3.2.2-bin.tar.gz | tar zx
-# export PATH=$PATH:apache-maven-3.2.2/bin
+echo Download project template
+git clone -q https://github.com/wildfly-swarm-openshiftio-boosters/wfswarm-health-check.git my-first-project
 
-# echo Downloading the project
-# unzip <(( curl -q https://github.com/wildfly-swarm-openshiftio-boosters/wfswarm-health-check/archive/7.0.0-redhat-3.zip ))
+echo Install image streams
+for i in {1..200}; do oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/ose-v1.4.5/jboss-image-streams.json -n openshift && break || sleep 1; done
