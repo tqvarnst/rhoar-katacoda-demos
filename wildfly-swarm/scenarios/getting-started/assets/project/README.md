fb15bd217f8fd1ffc3a4819cb76366f42cee3213
Thomas Qvarnström <tqvarnst@redhat.com>
2017-10-18 12:56:58 +0200

    test

--- wildfly-swarm/scenarios/getting-started/assets/project/README.md
+++
@@ -1,1 +0,0 @@
-http://appdev.prod-preview.openshift.io/docs/wf-swarm-runtime.html#mission-health-check-wf-swarm
--- wildfly-swarm/scenarios/getting-started/client-init.sh
+++ wildfly-swarm/scenarios/getting-started/client-init.sh
@@ -5,3 +5,10 @@
 #git checkout 7.0.0-redhat-2
 
 #curl -L -o wfproject.zip https://github.com/wildfly-swarm-openshiftio-boosters/wfswarm-health-check/archive/7.0.0-redhat-3.zip
+
+# echo Installing Maven
+# curl http://www-us.apache.org/dist/maven/binaries/apache-maven-3.2.2-bin.tar.gz | tar zx
+# export PATH=$PATH:apache-maven-3.2.2/bin
+
+# echo Downloading the project
+# unzip <(( curl -q https://github.com/wildfly-swarm-openshiftio-boosters/wfswarm-health-check/archive/7.0.0-redhat-3.zip ))
