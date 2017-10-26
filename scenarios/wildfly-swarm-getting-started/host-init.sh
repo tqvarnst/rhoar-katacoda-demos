bcb862fc706edf12167867eaa943089f643b7435
Thomas Qvarnström <tqvarnst@redhat.com>
2017-10-26 20:52:40 +0200

    test

--- scenarios/wildfly-swarm-getting-started/host-init.sh
+++ scenarios/wildfly-swarm-getting-started/host-init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ~/.launch.sh
 
-export PATH=$PATH:$(cd apache-maven*/bin && pwd)
+export PATH=$PATH:$(cd /root/apache-maven*/bin && pwd)
 
 git clone -q https://github.com/wildfly-swarm-openshiftio-boosters/wfswarm-health-check.git /root/project
