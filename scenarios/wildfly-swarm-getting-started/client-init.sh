32a8ee352c5ab68e4897eda918724be3615de237
Thomas Qvarnström <tqvarnst@redhat.com>
2017-10-26 09:56:27 +0200

    test

--- scenarios/wildfly-swarm-getting-started/client-init.sh
+++ scenarios/wildfly-swarm-getting-started/client-init.sh
@@ -8,11 +8,5 @@ echo Installing Maven
 curl -s http://www-us.apache.org/dist/maven/binaries/apache-maven-3.2.2-bin.tar.gz | tar zx
 export PATH=$PATH:apache-maven-3.2.2/bin
 
-# echo Download project template
-# git clone -q https://github.com/wildfly-swarm-openshiftio-boosters/wfswarm-health-check.git my-first-project
-
-#echo Unzipping project
-#tar -zxf project.tar.gz
-
 echo Install image streams
 for i in {1..200}; do oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/ose-v1.4.5/jboss-image-streams.json -n openshift && break || sleep 1; done
