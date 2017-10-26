5adbcdf6643daf8b664711047d0b4548ee04259f
Thomas Qvarnström <tqvarnst@redhat.com>
2017-10-26 10:13:20 +0200

    test

--- scenarios/wildfly-swarm-getting-started/client-init.sh
+++ scenarios/wildfly-swarm-getting-started/client-init.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 ~/.launch.sh
 
-echo Installing Java
-yum install -y -q -e 0 java-1.8.0-openjdk-devel
-
-echo Installing Maven
-curl -s http://www-us.apache.org/dist/maven/binaries/apache-maven-3.2.2-bin.tar.gz | tar zx
-export PATH=$PATH:apache-maven-3.2.2/bin
-
-echo Install image streams
-for i in {1..200}; do oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/ose-v1.4.5/jboss-image-streams.json -n openshift && break || sleep 1; done
--- scenarios/wildfly-swarm-getting-started/env-init.sh
+++ scenarios/wildfly-swarm-getting-started/env-init.sh
@@ -1,3 +1,5 @@
-#ssh root@host01 'for i in {1..200}; do oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/ose-v1.4.5/jboss-image-streams.json -n openshift && break || sleep 1; done'
+ssh root@host01 'yum install -y -q -e 0 java-1.8.0-openjdk-devel'
+ssh root@host01 'curl -s http://www-us.apache.org/dist/maven/binaries/apache-maven-3.2.2-bin.tar.gz | tar zx -C /root/ && echo "export PATH=\$PATH:apache-maven-3.2.2/bin" >> /root/.bash_profile'
+ssh root@host01 'for i in {1..200}; do oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/ose-v1.4.5/jboss-image-streams.json -n openshift && break || sleep 1; done'
 
 
