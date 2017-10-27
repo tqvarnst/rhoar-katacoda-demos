7a222c5b431503d191b4b9a92e52c011bf3fef4c
Thomas Qvarnström <tqvarnst@redhat.com>
2017-10-27 18:06:12 +0200

    Removed commented lines

--- scenarios/wildfly-swarm-getting-started/host-init.sh
+++ scenarios/wildfly-swarm-getting-started/host-init.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 ~/.launch.sh
 
-#export PATH=$PATH:/root/apache-maven-3.2.2/bin
-
 git clone -q https://github.com/wildfly-swarm-openshiftio-boosters/wfswarm-health-check.git /root/project
