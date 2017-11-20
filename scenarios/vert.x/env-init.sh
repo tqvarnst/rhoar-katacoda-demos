57666630e7394181f30b8b7ad4f044678a954e97
Thomas Qvarnström <tqvarnst@redhat.com>
2017-11-20 13:09:00 +0100

    Added mvn go-offline

--- scenarios/vert.x/env-init.sh
+++ scenarios/vert.x/env-init.sh
@@ -1,1 +1,2 @@
-ssh root@host01 "git clone -q https://github.com/tqvarnst/vertx-health-checks-booster-redhat.git /root/project"
+ssh root@host01 "git clone -q https://github.com/openshiftio-vertx-boosters/vertx-http-booster-redhat.git /root/project"
+ssh root@host01 "cd /root/project && mvn dependency:go-offline"
