481045b73c0bcd807125e3e0f557ff02055990f2
Thomas Qvarnström <tqvarnst@redhat.com>
2017-11-20 12:42:54 +0100

    Move clone to background script

---
+++ scenarios/vert.x/env-init.sh
@@ -0,0 +1,1 @@
+ssh root@host01 "git clone -q https://github.com/tqvarnst/vertx-health-checks-booster-redhat.git /root/project"
--- scenarios/vert.x/host-init.sh
+++ scenarios/vert.x/host-init.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 ~/.launch.sh
 
-git clone -q https://github.com/tqvarnst/vertx-health-checks-booster-redhat.git /root/project
