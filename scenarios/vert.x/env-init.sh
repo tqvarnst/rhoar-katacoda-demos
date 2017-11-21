47a6a857346659f449673a92a51e97e4a7124fd0
Thomas Qvarnström <tqvarnst@redhat.com>
2017-11-21 17:54:51 +0100

    test

--- scenarios/vert.x/env-init.sh
+++ scenarios/vert.x/env-init.sh
@@ -1,2 +1,2 @@
 ssh root@host01 "git clone -q https://github.com/tqvarnst/vertx-http-booster-redhat.git /root/project"
-ssh root@host01 "cd /root/project && nohup mvn dependency:go-offline &"
+#ssh root@host01 "cd /root/project && nohup mvn dependency:go-offline &"
